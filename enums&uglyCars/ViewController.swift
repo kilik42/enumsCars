3deeb8c760216b162b01f56871a6379ddcf4e4e8
marvin evins <maevins@cps.edu>
2016-07-09 08:11:16 -0500

    enums with cars swift

--- enums&uglyCars/ViewController.swift
+++ enums&uglyCars/ViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var thelabel: UILabel!
 
+    
+    enum Cars: Int{
+        
+        case BMW = 0
+        case Honda = 1
+        case TESLA = 2
+        case SUBARU = 3
+    }
+    
+    
+    var carOfChoice: Cars!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onBtnClickTapped(sender: UIButton!) {
+        
+        if sender.tag == Cars.BMW.rawValue {
+            thelabel.text = "bmw is awesome"
+        }else if sender.tag == Cars.Honda.rawValue{
+            thelabel.text = "somebody ge that ugly fin out of my face"
+        } else if sender.tag == Cars.TESLA.rawValue{
+            thelabel.text = "the new cool kid on the block!"
+        } else if sender.tag == Cars.SUBARU.rawValue {
+            thelabel.text = "suburu is heere to stay"
+        }else {
+            thelabel.text = "somebody shoot me now!"
+        }
     }
 
 
